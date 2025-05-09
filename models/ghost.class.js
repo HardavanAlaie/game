f46d011ca4eb943619094f8aa22a602e1c43a9f2
Hardavan Alaie <h.alaie@gmx.de>
2025-05-09 19:45:28 +0200

    animated enemy ghost

--- models/ghost.class.js
+++ models/ghost.class.js
@@ -1,13 +1,28 @@
-class Ghost extends MovableObject{
+class Ghost extends MovableObject {
+  IMAGES_RUNNING = [
+'img/ghost/ghost01_idle1.png',
+'img/ghost/ghost02_idle2.png',
+'img/ghost/ghost03_idle3.png',
+'img/ghost/ghost04_walk_turn.png',
+'img/ghost/ghost05_walk1.png',
+'img/ghost/ghost06_walk2.png',
+'img/ghost/ghost07_walk3.png',
+'img/ghost/ghost08_walk4.png',
+  ];
+  
+  currentImage = 0;
 
-    constructor() {
-        super().loadImage('img/ghost.png');
+  constructor() {
+    super().loadImage("img/ghost.png");
+    this.loadImages(this.IMAGES_RUNNING);
 
-        this.x = 200 + Math.random() * 500;
-        //this.animate();
-    }
+    this.animate();
 
-      animate() {
+    this.x = 200 + Math.random() * 500;
+    //this.animate();
+  }
+
+  animate() {
     setInterval(() => {
       let i = this.currentImage % this.IMAGES_RUNNING.length;
       let path = this.IMAGES_RUNNING[i];
@@ -15,10 +30,10 @@ class Ghost extends MovableObject{
       this.currentImage++;
     }, 100);
   }
-  
-    // animate() {
-    //     setInterval(() => {
-    //         this.x -= 0.2;
-    //     }, 1000 / 60);
-    // }
+
+  // animate() {
+  //     setInterval(() => {
+  //         this.x -= 0.2;
+  //     }, 1000 / 60);
+  // }
 }
