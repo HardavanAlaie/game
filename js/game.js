553cea759d0cb8fd9cce903c602a568ae8daa03d
Hardavan Alaie <h.alaie@gmx.de>
2025-05-10 21:28:20 +0200

    new class Keyboard

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
@@ -10,3 +11,42 @@ function init() {
     console.log('My Character', world['character']);
     console.log('The Enemie', world.enemies);
 }
+
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode == 37) {
+        keyboard.LEFT = true;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = true;
+    }
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = true;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = true;
+    }
+    if (e.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+    console.log(e);
+});
+
+window.addEventListener("keyup", (e) => {
+    if (e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+    if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+});
+    
+
