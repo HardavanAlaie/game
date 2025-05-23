4e766e53afb3d5b068a2825b5acf1c090d8a5db8
Hardavan Alaie <h.alaie@gmx.de>
2025-05-23 17:10:20 +0200

    new functions flipImage() and flipImageBack()

--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -37,8 +37,16 @@ class MovableObject {
     });
   }
 
-  draw() {
-    this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = '5';
+    ctx.strokeStyle = 'blue';
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
   }
 
   playAnimation(images) {
