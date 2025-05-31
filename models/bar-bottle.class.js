b97edf176dc500debd2f9c9bdc4ac0ef57bc9191
Hardavan Alaie <h.alaie@gmx.de>
2025-05-31 20:41:32 +0200

    new classes bar bottle and throwable

--- models/bar-bottle.class.js
+++ models/bar-bottle.class.js
@@ -1,12 +1,43 @@
-class Bottle extends DrawableObject {
+class BottleBar extends DrawableObject {
+  IMAGES = [
+    "img/gameui/bottle/0.png",
+    "img/gameui/bottle/20.png",
+    "img/gameui/bottle/40.png",
+    "img/gameui/bottle/60.png",
+    "img/gameui/bottle/80.png",
+    "img/gameui/bottle/100.png",
+  ];
 
+  availableBottle = 0;
 
-    IMAGES = [
-        'img/gameui/bottle/0.png',
-        'img/gameui/bottle/20.png',
-        'img/gameui/bottle/40.png',
-        'img/gameui/bottle/60.png',
-        'img/gameui/bottle/80.png',
-        'img/gameui/bottle/100.png',
-    ]
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES);
+    this.x = 60;
+    this.y = 20;
+    this.width = 200;
+    this.height = 60;
+  }
+
+    setPercentage(availableBottle) {
+    this.availableBottle = availableBottle;
+    let path = this.IMAGES[this.resolveImageIndex()];
+    this.img = this.imageCache[path];
+  }
+
+  resolveImageIndex() {
+    if (this.percentage == 100) {
+      return 5;
+    } else if (this.percentage > 80) {
+      return 4;
+    } else if (this.percentage > 60) {
+      return 3;
+    } else if (this.percentage > 40) {
+      return 2;
+    } else if (this.percentage > 20) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
---
+++ models/throwable-object.class.js
@@ -0,0 +1,5 @@
+class ThrowableObject extends DrawableObject {
+    constructor() {
+        super();
+    }
+}
